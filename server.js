95c4877ab8c0c6d19c0d533135e6f6fe8e24da9a
solowon27 <122332373+solowon27@users.noreply.github.com>
2023-06-30 16:08:42 -0600

    get and post request added to return json data

--- server.js
+++ server.js
@@ -5,6 +5,23 @@ const fs = require('fs');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+app.get('/api/notes', (req, res) => { // get request to get all notes
+  const notes = getNotes();
+  res.json(notes);
+});
+
+app.post('/api/notes', (req, res) => { // post request to add a note
+  const newNote = req.body;
+  addNote(newNote);
+  res.json(newNote);
+});
+
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
