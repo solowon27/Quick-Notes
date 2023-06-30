3151636dc0a15cf27464074d84016a0dc0780e7a
solowon27 <122332373+solowon27@users.noreply.github.com>
2023-06-30 16:01:52 -0600

    create a server to

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
