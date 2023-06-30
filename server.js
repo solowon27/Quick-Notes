9fc49c390839b8ddf9ccc043d5e63b4c07c0e076
solowon27 <122332373+solowon27@users.noreply.github.com>
2023-06-30 16:18:31 -0600

    fetchNote, addNote and saveNote functions added to server.js

--- server.js
+++ server.js
@@ -11,16 +11,40 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.get('/api/notes', (req, res) => { // get request to get all notes
-  const notes = getNotes();
+  const notes = fetchNote();
   res.json(notes);
 });
 
 app.post('/api/notes', (req, res) => { // post request to add a note
-  const newNote = req.body;
+  const newNote = req.body; // new note from client side
   addNote(newNote);
   res.json(newNote);
 });
 
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html')); // get request to get/return notes.html
+  });
+  
+  app.get('*', (req, res) => { // get request to get/return index.html
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
+  
+  function fetchNote() {
+    const data = fs.readFileSync(path.join(__dirname, './db/db.json'), 'utf8');
+    return JSON.parse(data);
+  }
+  
+  function addNote(newNote) {
+    const notes = fetchNote();
+    notes.push(newNote);
+    saveNotes(notes);
+  }
+  
+
+    function saveNotes(notes) {
+        fs.writeFileSync(path.join(__dirname, './db/db.json'), JSON.stringify(notes, null, 2));
+      }
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
