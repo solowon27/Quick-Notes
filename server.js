10bd97818ca982cd54f2973402b17dfcfa4c8acf
solowon27 <122332373+solowon27@users.noreply.github.com>
2023-06-30 17:43:35 -0600

    comments added to server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const fs = require('fs');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000; //process.env.PORT is for heroku deployment
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.get('/api/notes', (req, res) => { // get request to get all notes
-  const notes = fetchNote();
+  const notes = getNotes();
   res.json(notes);
 });
 
@@ -21,6 +21,11 @@ app.post('/api/notes', (req, res) => { // post request to add a note
   res.json(newNote);
 });
 
+app.delete('/api/notes/:id', (req, res) => { // delete request to delete a note
+    const noteId = req.params.id;
+    deleteNote(noteId);
+    res.sendStatus(200);
+  });
 
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html')); // get request to get/return notes.html
@@ -30,19 +35,24 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
   });
   
-  function fetchNote() {
+  function getNotes() {
     const data = fs.readFileSync(path.join(__dirname, './db/db.json'), 'utf8');
     return JSON.parse(data);
   }
   
-  function addNote(newNote) {
-    const notes = fetchNote();
+  function addNote(newNote) { // add new note to db.json by pushing new note to notes array
+    const notes = getNotes();
     notes.push(newNote);
     saveNotes(notes);
   }
   
+  function deleteNote(noteIndex) { // delete note from db.json
+      const notes = getNotes();
+      notes.splice(noteIndex, 1);
+      saveNotes(notes);
+    }
 
-    function saveNotes(notes) {
+    function saveNotes(notes) { // save notes to db.json by writing to db.json
         fs.writeFileSync(path.join(__dirname, './db/db.json'), JSON.stringify(notes, null, 2));
       }
 
